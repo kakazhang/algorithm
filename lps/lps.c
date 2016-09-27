83ad13f30ba77fefde85d426cedd2b3a6d8dbdae
kakazhang <kakazhang@qiyi.com>
2016-09-27 16:10:25 +0800

    find longest substring which is panlindromic in a given string

---
+++ lps/lps.c
@@ -0,0 +1,35 @@
+/*************************************************************************
+	> File Name: lps.c
+	> Author: 
+	> Mail: 
+	> Created Time: 2016年09月27日 星期二 15时01分17秒
+ ************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+#define max(a,b) ( (a) > (b) ? (a) : (b))
+
+int lps(const char* str, int l, int r) {
+    if (l == r)
+        return 1;
+
+    if ((str[l]==str[r]) && l+1==r) {
+        return 2;
+    }
+
+    if (str[l] == str[r])
+        return lps(str, l+1, r-1) + 2;
+
+    return max(lps(str, l, r-1), lps(str, l+1, r));
+}
+
+int main() {
+    char str[] = {"geeksforgeeks"};
+    int len = sizeof(str)/sizeof(str[0]);
+
+    int count = lps(str, 0, len-1);
+
+    printf("lps(%s) is %d\n", "kaka", count);
+
+    return 0;
+}
---
+++ lps/readMe.txt
@@ -0,0 +1,1 @@
+Given a string, find the longest panlindromic substring.
