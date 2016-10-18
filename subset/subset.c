ff74a06ab930b9618d58b664e70bec1ec2a50b59
kakazhang <kakazhang@qiyi.com>
2016-10-18 16:44:50 +0800

    show all subsets of an array

---
+++ subset/subset.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void allSubset(int arr[], int n) {
+    int i, j;
+
+    for (i = 0; i < (1 << n); i++) {
+       printf("{");
+
+       for (j = 0; j < n; j++) {
+           if ( (i & (1 << j)) ) {
+               printf("%d ", arr[j]);
+           }
+       }
+       printf("}\n");
+    }
+}
+
+int main() {
+    int arr[] = {2, 5, 29, 10};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    allSubset(arr, n);
+
+    return 0;
+}
