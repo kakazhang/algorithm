85901d46fa56adf7dd7eb39c0a5b50396e0b9120
zhanglin <zhanglin3406@gmail.com>
2016-10-29 22:33:13 +0800

    add swap of strings

--- qualcomm_interview/main.c
+++ qualcomm_interview/main.c
@@ -8,6 +8,7 @@
 
 extern void swap_add(int *pa, int *pb);
 extern void swap_xor(int *pa, int *pb);
+extern void swap_str(char str1[], char str2[]);
 
 int main(int argc, char** argv) {
     int a = atoi(argv[1]);
@@ -18,5 +19,12 @@ int main(int argc, char** argv) {
     swap_xor(&a, &b);
     printf("a:%d,b:%d\n", a, b);
 
+    char str1[24] = {"kaka back"};
+    char str2[24] = {"algorithm"};
+
+    printf("str1:%s,str2:%s\n", str1, str2);
+    swap_str(str1, str2);
+    printf("str1:%s,str2:%s\n", str1, str2);
+    
     return 0;
 }
--- qualcomm_interview/swap.c
+++ qualcomm_interview/swap.c
@@ -4,6 +4,9 @@
 	> Mail: 
 	> Created Time: 2016年10月14日 星期五 17时01分57秒
  ************************************************************************/
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 void swap_add(int *pa, int *pb) {
     *pa = *pa + *pb;
@@ -15,4 +18,16 @@ void swap_xor(int *pa, int *pb) {
     *pa = *pa ^ *pb;
     *pb = *pa ^ *pb;
     *pa = *pa ^ *pb;
+}
+
+void swap_str(char str1[], char str2[]) {
+    int len = strlen(str1);
+
+    char *tmp = (char *)malloc(len+1);
+
+    strcpy(tmp, str1);
+    strcpy(str1, str2);
+    strcpy(str2, tmp);
+
+    return;
 }
