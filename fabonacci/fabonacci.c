cea10d082ea1929b16d3601a1003009bc0e5588e
zhanglin <zhanglin3406@gmail.com>
2016-11-16 21:46:40 +0800

    find nth even fabonacci number

---
+++ fabonacci/fabonacci.c
@@ -0,0 +1,37 @@
+/*************************************************************************
+	> File Name: fabonacci.c
+	> Author: 
+	> Mail: 
+	> Created Time: 2016年11月16日 星期三 21时24分44秒
+    >Description:given a number n,find the nth even fabonacci number
+ ************************************************************************/
+#include <stdio.h>
+
+int fabonacci(unsigned int n) {
+    if (n == 0)
+        return 0;
+    else if (n == 1)
+        return 1;
+
+    return fabonacci(n-2) + fabonacci(n-1);
+}
+
+int get_even_number(int n) {
+    int index = 3 * n;
+
+    return fabonacci(index);
+}
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        printf("please input a number\n");
+        return -1;
+    }
+
+    int n = atoi(argv[1]);
+
+    int number = get_even_number(n);
+    printf("%d th number is %d\n", n, number);
+
+    return 0;
+}
