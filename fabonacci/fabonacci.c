b79bf0f6497690a2fc14559ebf33cedcb9e5be90
zhanglin <zhanglin3406@gmail.com>
2016-11-22 21:20:51 +0800

    optimize recurion with iteration

--- fabonacci/fabonacci.c
+++ fabonacci/fabonacci.c
@@ -6,6 +6,21 @@
     >Description:given a number n,find the nth even fabonacci number
  ************************************************************************/
 #include <stdio.h>
+/**
+*use iteration algorithm to optimize recursion algorithm
+* */
+int optimise(unsigned int n) {
+    int i;
+    int arr[n+1];
+    arr[0] = 0;
+    arr[1] = 1;
+
+    for (i = 2; i <= n; i++) {
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+
+    return arr[n];
+}
 
 int fabonacci(unsigned int n) {
     if (n == 0)
@@ -19,7 +34,8 @@ int fabonacci(unsigned int n) {
 int get_even_number(int n) {
     int index = 3 * n;
 
-    return fabonacci(index);
+    //return fabonacci(index);
+    return optimise(index);
 }
 
 int main(int argc, char **argv) {
