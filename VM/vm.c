bbf0255fd54a8a9044ca12da4c296b717932c776
zhanglin <zhanglin3406@gmail.com>
2016-12-05 22:02:48 +0800

    a small VM of version0.1

---
+++ VM/vm.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_SIZE 24
+#define DEFAULT_SIZE 8
+
+typedef enum type {
+    TYPE_INT = 0,
+	TYPE_PAIR,
+} Type;
+
+typedef struct object {
+   Type type;
+
+   union {
+       struct object* lchild;
+	   struct object* rchild;
+
+	   int val;
+   }ele;
+
+   struct object* next;
+}Object, *pObject;
+
+typedef struct vm {
+   int capacity;
+   int count;
+
+   pObject* objects;
+   pObject root;
+} VM, *pVM;
+
+static void assert(int cond, const char* msg) {
+    if (!cond) {
+        printf("%s\n", msg);
+		exit(-1);
+	}
+}
+
+static pObject alloc(Type type) {
+    pObject obj = (pObject)malloc(sizeof(Object));
+	assert((obj != NULL), "no space for object");
+
+	obj->type = type;
+	obj->next = NULL;
+	
+	return obj;
+}
+
+static void destroy(pObject obj) {
+    if (obj)
+		free(obj);
+}
+
+pObject pop(pVM vm) {
+    if (vm->count == 0)
+		return NULL;
+
+	return vm->objects[--vm->count];
+}
+
+void push(pVM vm, int val) {
+    if (vm->count >= vm->capacity) {
+		printf("full\n");
+		return;
+	}
+
+	pObject obj = alloc(TYPE_INT);
+	obj->ele.val = val;
+    obj->next = vm->root;
+	vm->root = obj;
+
+	vm->objects[vm->count++] = obj;
+}
+
+void push_pair(pVM vm) {
+    pObject obj = alloc(TYPE_PAIR);
+	obj->ele.lchild = pop(vm);
+	obj->ele.rchild = pop(vm);
+
+    vm->objects[vm->count++] = obj;
+}
+
+pVM init(int maxsize) {
+    if (maxsize > MAX_SIZE)
+		maxsize = MAX_SIZE;
+
+	pVM vm = (pVM)malloc(sizeof(VM));
+	assert((vm != NULL), "no space for VM");
+
+	vm->capacity = maxsize;
+	vm->count = 0;
+
+	vm->objects = (pObject)malloc(sizeof(pObject) * vm->capacity);
+	assert((vm->objects != NULL), "no space for objects");
+
+    vm->root = NULL;
+	
+	return vm;
+}
+
+void freeAll(pVM vm) {
+    if (vm) {
+        pObject obj = vm->root;
+        pObject tmp = NULL;
+		while (obj) {
+            tmp = obj;
+			obj = obj->next;
+
+			free(tmp);
+		}
+
+		free(vm);
+	}
+}
+
+void show(pVM vm) {
+    if (vm) {
+        pObject obj = vm->root;
+        pObject tmp = NULL;
+		while (obj) {
+            tmp = obj;
+			obj = obj->next;
+			
+            if (tmp ->type == TYPE_INT)
+				printf("val:%d\n", tmp->ele.val);
+			else
+				printf("obj:%d\n", (int)tmp->type);
+		}
+	}
+}
+
+int main() {
+   pVM vm = init(DEFAULT_SIZE);
+   
+   push(vm, 3);
+   push(vm, 2);
+   push(vm, 21);
+   push_pair(vm);
+   push(vm, 7);
+   
+   show(vm);
+   freeAll(vm);
+
+   return 0;
+}
