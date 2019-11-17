896e836e32994bd3601b3a2526ad743519aa143c
zhanglin <zhanglin3406@gmail.com>
2019-11-17 16:33:18 +0800

    add recursion implementation of euclid algorithm

--- euclid/euclid.c
+++ euclid/euclid.c
@@ -6,6 +6,15 @@
  ************************************************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
+
+int change(int* x, int* y) {
+    int tmp = *x;
+	*x = *y;
+	*y = tmp;
+
+	return 0;
+}
 
 int gcd(int x, int y) {
     while (x != y) {
@@ -18,11 +27,28 @@ int gcd(int x, int y) {
     return y;
 }
 
+
+int euclid(int x, int y) {
+	int r;
+    if (y > x) 
+        change(&x, &y);
+
+	r = x % y;
+	if (r == 0) 
+        return y;
+
+    x = y;
+    y = r;
+
+	return euclid(x, y);
+}
+
 int main(int argc, char** argv) {
     int x = atoi(argv[1]);
     int y = atoi(argv[2]);
 
     printf("gcd(%d,%d)=%d\n", x, y, gcd(x,y));
+    printf("gcd(%d,%d)=%d\n", x, y, euclid(x,y));
 
     return 0;
 }
